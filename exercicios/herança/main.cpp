f5a1578bee8f297d249dd9dc284d40605b4d9eef
Gabriel Freitas <65826668+gabfreitassilva@users.noreply.github.com>
2022-10-31 10:48:20 -0300

    exercicio

---
+++ exercicios/herança/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+class Veiculos {
+    private:
+        int rodas, passageiros;
+    public:
+        void fixaRodas(int valor);
+        int obtemRodas();
+        void fixaPassageiros(int valor);
+        int obtemPassageiros();
+};
+
+void Veiculos::fixaRodas(int valor) {
+    rodas = valor;
+}
+int Veiculos::obtemRodas() {
+    return rodas;
+}
+
+void Veiculos::fixaPassageiros(int valor) {
+    passageiros = valor;
+}
+int Veiculos::obtemPassageiros() {
+    return passageiros;
+}
+
+class Caminhao : public Veiculos {
+    private:
+        int carga;
+    public:
+        void fixaCarga(int valor);
+        int obtemCarga();
+        void exibir();
+};
+
+void Caminhao::fixaCarga(int valor) {
+    carga = valor;
+}
+int Caminhao::obtemCarga() {
+    return carga;
+}
+void Caminhao::exibir() {
+    cout << "Rodas: " << obtemRodas() << '\n';
+    cout << "Passageiros: " << obtemPassageiros() << '\n';
+    cout << "Carga: " << obtemCarga() << '\n';
+}
+
+enum tipo {carro, furgao, perua};
+class Automovel : public Veiculos {
+    private:
+        enum tipo tipoCarro;
+    public:
+        void fixaTipo(enum tipo t);
+        enum tipo obtemTipo();
+        void exibir();
+};
+
+void Automovel::fixaTipo(enum tipo t) {
+    tipoCarro = t;
+}
+enum tipo Automovel::obtemTipo() {
+    return tipoCarro;
+}
+void Automovel::exibir() {
+    cout << "Rodas: " << obtemRodas() << '\n';
+    cout << "Passageiros: " << obtemPassageiros() << '\n';
+    cout << "Tipo: ";
+    switch (obtemTipo()) {
+        case carro:
+            cout << "carro\n";
+            break;
+        case furgao:
+            cout << "furgao\n";
+            break;
+        case perua:
+            cout << "perua\n";
+            break;
+    }
+}
+
+class Motocicleta : public Veiculos {
+    private:
+        
+};
+
+int main() {
+    Caminhao caminhao[2];
+    int rodas, passageiros, carga;
+
+    cout << "CAMINHAO: 1\n";
+    cout << "Rodas: ";
+    cin >> rodas;
+    cout << "Passageiros: ";
+    cin >> passageiros;
+    cout << "Carga: ";
+    cin >> carga;
+
+    caminhao[0].fixaRodas(rodas);
+    caminhao[0].fixaPassageiros(passageiros);
+    caminhao[0].fixaCarga(carga);
+    cout << "-----------------CAMINHAO CADASTRADO---------------\n";
+    caminhao[0].exibir();
+    cout << "---------------------------------------------------\n";
+
+    cout << "CAMINHAO: 2\n";
+    cout << "Rodas: ";
+    cin >> rodas;
+    cout << "Passageiros: ";
+    cin >> passageiros;
+    cout << "Carga: ";
+    cin >> carga;
+
+    caminhao[1].fixaRodas(rodas);
+    caminhao[1].fixaPassageiros(passageiros);
+    caminhao[1].fixaCarga(carga);
+    cout << "-----------------CAMINHAO CADASTRADO---------------\n";
+    caminhao[1].exibir();
+    cout << "---------------------------------------------------\n";
+
+    return 0;
+}
