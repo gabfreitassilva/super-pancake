4f7a614274b3e560f6d3794c707332a911e5ae09
Gabriel Freitas <65826668+gabfreitassilva@users.noreply.github.com>
2022-12-07 07:34:30 -0300

    Update ordemAlfabetica.cpp

--- ifce/ordemAlfabetica.cpp
+++ ifce/ordemAlfabetica.cpp
@@ -13,13 +13,19 @@ int main() {
     cursos.push("etica e Cidadania");
     cursos.push("metrologia");
     cursos.push("calculo");
-
+    
+    // laço para popular o vector com os itens da pilha
     for (int j = 0; j < 5; j++) {
         ordemAlfabetica.push_back(cursos.top());
         cursos.pop();
     }
-    std::sort(ordemAlfabetica.begin(), ordemAlfabetica.end());
-
+    std::sort(ordemAlfabetica.begin(), ordemAlfabetica.end()); // função sort usada para a ordenação alfabetica (biblioteca algorithm)
+    /*
+    primeiro membro da função você informa o começo do vector/vetor
+    já no segundo membro você informa o fim do vetor/vector
+    */
+    
+    // saida em ordem alfabetica
     std::cout << "Cursos cadastrados na pilha (ordem alfabetica):\n";
     for (int m = 0; m < 5; m++) {
         std::cout << "Curso " << (m + 1) << ": " << ordemAlfabetica[m] << '\n';
